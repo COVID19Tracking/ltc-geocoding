fca5dd979d0e82b695ad5d02f0ff9823fc2806d9
Jonathan Gilmour <jonatgilmour@gmail.com>
2020-12-18 19:15:39 -0500

    initial hash code

---
+++ create_ltc_ids.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 17:51:57 2020
+
+@author: Gilmourj
+"""
+
+import numpy as np
+import pandas as pd
+
+ltc = pd.read_json("~/python/website/_data/long_term_care_facilities.json")
+df = ltc[['state', 'county', 'city', 'facility_name']]
+df['hash'] = df.apply(lambda x: hash(tuple(x)), axis = 1)
