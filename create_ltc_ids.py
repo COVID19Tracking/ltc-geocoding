3817b082e18ae8065492b752dc23bdfb00455b6b
Jonathan Gilmour <jonatgilmour@gmail.com>
2020-12-21 20:50:35 -0500

    cleaning up file, adding json with hash IDs

--- create_ltc_ids.py
+++ create_ltc_ids.py
@@ -9,6 +9,17 @@ Created on Fri Dec 18 17:51:57 2020
 import numpy as np
 import pandas as pd
 
+
 ltc = pd.read_json("~/python/website/_data/long_term_care_facilities.json")
-df = ltc[['state', 'county', 'city', 'facility_name']]
-df['hash'] = df.apply(lambda x: hash(tuple(x)), axis = 1)
+
+def create_hash():
+    df = ltc[['state', 'county', 'city', 'facility_name','facility_type_state']]
+    df['hash'] = df.apply(lambda x: hash(tuple(x)), axis = 1)
+    print(df['hash'].duplicated().describe())
+    df.to_json('ltc_hashed.json')
+
+def main():
+    create_hash()
+
+if __name__ == "__main__":
+    main()
