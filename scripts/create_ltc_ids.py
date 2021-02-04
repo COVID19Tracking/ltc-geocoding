362a7f597b8892fa7bbef7cd3701b5df8b8a8c3f
Jonathan Gilmour <jonatgilmour@gmail.com>
2021-02-04 14:42:46 -0500

    ugly code to accommodate merging old results

--- scripts/create_ltc_ids.py
+++ scripts/create_ltc_ids.py
@@ -28,8 +28,13 @@ def drop_dupes(df):
     return df
 
 def create_hash(record):
-    record['hash'] = hash(tuple(record))
+    record['hash'] = hash(tuple(record[['state', 'county', 'city', 'facility_name']]))
     return record
+
+def uppercase(df):
+    for colname in ['state', 'county', 'city', 'facility_name']:
+        df[colname] = df[colname].str.upper()
+    return df
 
 def build_query(record):
     query = ""
@@ -44,11 +49,9 @@ def build_query(record):
     return query
 
 def geocode(record):
-    if(not pd.isnull(record['lat'])):
+    if(not record['lat'] == ''):
         return record
-
     query = build_query(record)
-
     try:
         result = gmaps.geocode(query)
     except Exception as err:
@@ -72,10 +75,23 @@ def geocode(record):
 
 def main():
     df = ltc[['state', 'county', 'city', 'facility_name']]
+    df = df.fillna(value='')
     df = drop_dupes(df)
     df = df.apply(create_hash, axis = 1)
-    df = df.apply(geocode, axis = 1)
-    df.to_csv('ltc_geocoded_hashed.csv')
+    ltc_geo = ltc_geo.drop('Unnamed: 0', axis = 1)
+    ltc_geo = ltc_geo.fillna(value='')
+    ltc_geo_upper = ltc_geo.drop('hash', axis = 1)
+    ltc_geo_upper = uppercase(ltc_geo_upper)
+    ltc_geo_upper = ltc_geo_upper.apply(create_hash, axis = 1)
+    merged = df.merge(ltc_geo.set_index('hash'), how='outer')
+    merged_2 = merged.merge(ltc_geo_upper.set_index('hash'), how='outer')
+    merged_2 = drop_dupes(merged_2)
+    merged_2 = merged_2.drop('hash', axis = 1)
+    merged_2 = merged_2.fillna(value='')
+    merged_2 = merged_2.apply(create_hash, axis = 1)
+    geo = merged_2.apply(geocode, axis = 1)
+    geo = drop_dupes(geo)
+    geo.to_csv('ltc_geocoded_hashed.csv')
 
 if __name__ == "__main__":
     main()
