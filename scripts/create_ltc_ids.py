62dc5d1ff4bdae5e0b1f5c4a3a18e6db946a6659
Jonathan Gilmour <jonatgilmour@gmail.com>
2021-02-09 19:29:34 -0500

    writing geocoded file

--- scripts/create_ltc_ids.py
+++ scripts/create_ltc_ids.py
@@ -108,7 +108,7 @@ def main():
     geocoded = geocoded.drop_duplicates()
 
     # writing file
-    merged.to_csv('../ltc_geocoded_hashed.csv')
+    geocoded.to_csv('../ltc_geocoded_hashed.csv')
 
 
 if __name__ == "__main__":
